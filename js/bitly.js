01e24b8ed61d489f15347f23b2963da39f30c984
t-kit <40701045+t-kit@users.noreply.github.com>
2018-09-01 20:50:07 +0300

    Add files via upload

---
+++ js/bitly.js
@@ -0,0 +1,52 @@
+var bitly_url = 'http://api.bitly.com/v3/shorten';
+var bitly_login = 'o_71goo47q1b';
+var bitly_apiKey = 'R_e0131f8b722b446996d6c8595e1d7c9c';
+
+/* proxy */
+
+var shortenUrl = function(longUrl){
+    return $.Deferred(function(defer){
+        var action = bitly_url;
+        var data = {
+            login: bitly_login,
+            apiKey: bitly_apiKey,
+            format: 'json',
+            longUrl: longUrl
+        };
+        $.ajax(action, {
+            dataType: 'jsonp',
+            data: data
+        }).then(function(res){
+            if(!res || !res.data || !res.data.url){
+                reject();
+                return;
+            }
+            defer.resolve(res.data.url);
+        }, reject);
+        function reject(){
+            defer.reject({ msg: 'bit.ly не отвечает' });
+        }
+    }).promise();
+};
+
+/* DOM things */
+
+$(function(){
+
+    var $longUrl = $('#longUrl');
+    var $button = $('#button');
+    var $res = $('#longUrl');
+
+    $button.click(function(){
+        var longUrl = $longUrl.val();
+        if(longUrl === '') {
+            return;
+        }
+        shortenUrl(longUrl).then(function(shortUrl){
+            $res.text(shortUrl);
+        }, function(){
+            alert('Проверьте правильность ввода целевого URL (или ресурс bit.ly недоступен...)');
+        });
+    });
+
+});
---
+++ js/common.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+    $(function() {
+        $('form').submit(function() {
+            return false;
+        })
+    })
+
+    $('#editSource').click(function() {
+        if (this.checked) {
+            $('#utm_source').prop('disabled', false);   
+        } else {
+            $('#utm_source').prop('disabled', true);                 
+        }
+    });
+    $('#editMedium').click(function() {
+        if (this.checked) {
+            $('#utm_medium').prop('disabled', false);    
+        } else {
+            $('#utm_medium').prop('disabled', true);              
+        }
+    });
+
+    $('.data-submit').click(function() {
+        var url = $('#url').val();
+        var source = $('#utm_source').val();
+        var medium = $('#utm_medium').val();
+        var campaign = $('#utm_campaign').val();
+
+        $('.data-output, #longUrl').text(
+            url + "/?utm_source=" + 
+            source + "&utm_medium=" + 
+            medium + "&utm_campaign=" + 
+            campaign); 
+    });
+
+    function ClipBoard() {
+        new ClipboardJS('#copyShortened');
+    }
+    ClipBoard();
+
+    function checkValidity() {
+
+        var url = $('#url').val();
+        var source = $('#utm_source').val();
+        var medium = $('#utm_medium').val();
+        var campaign = $('#utm_campaign').val();
+
+        $('#build').click(function() {
+            if ($('#url').val() == '') {
+                alert('Поле «Целевой URL» не заполнено!»');
+            } else if ($('#utm_source').val() == '') {
+                alert('Поле «Источник трафика» не заполнено!»');
+            } else if ($('#utm_medium').val() == '') {
+                alert('Поле «Тип трафика» не заполнено!»');
+            } else if ($('#utm_campaign').val() == '') {
+                alert('Поле «Название кампании» не заполнено!»');
+            }
+
+        })
+    };
+    checkValidity();
+})
+
